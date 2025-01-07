75f3708ab6940a4113a850ef27612d1e6adea89f
민근 김 <bbbababb1215@naver.com>
2025-01-07 16:24:52 +0900

    초기 커밋.

---
+++ MySchoolScoreManagementUI/Applications/Controllers/ApplicationController.cs
@@ -0,0 +1,33 @@
+﻿using MySchoolScoreManagementUI.Applications.ViewModels;
+using System.ComponentModel.Composition;
+
+namespace MySchoolScoreManagementUI.Applications.Controllers
+{
+    [Export]
+    internal class ApplicationController
+    {
+        private readonly ShellViewModel shellViewModel;
+
+
+        [ImportingConstructor]
+        public ApplicationController(ShellViewModel shellViewModel)
+        {
+            this.shellViewModel = shellViewModel;
+        }
+
+
+
+        public void Initialize()
+        {
+        }
+
+        public void Run()
+        {
+            shellViewModel.Show();
+        }
+
+        public void Shutdown()
+        {
+        }
+    }
+}
---
+++ MySchoolScoreManagementUI/Presentation/Views/ShellWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MySchoolScoreManagementUI.Applications.Views;
+using System.ComponentModel.Composition;
+using System.Windows;
+
+namespace MySchoolScoreManagementUI.Presentation.Views
+{
+    [Export(typeof(IShellView))]
+    public partial class ShellWindow : Window, IShellView
+    {
+        public ShellWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
