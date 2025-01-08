174e5aa9a5f687f3e4bc0258d8819a37ae344313
민근 김 <bbbababb1215@naver.com>
2025-01-08 15:42:52 +0900

    Tree Node

--- MySchoolScoreManagementUI/Domain/ModelManger.cs
+++ MySchoolScoreManagementUI/Domain/ModelManger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Waf.Foundation;
@@ -15,10 +16,14 @@ namespace MySchoolScoreManagementUI.Domain
 
          protected ModelManger()
         {
-
+            SchoolYears.CollectionChanged += SchoolYearCollectionChanged;
         }
 
+
         public ObservableCollection<SchoolYear> SchoolYears { get; } = new ObservableCollection<SchoolYear>();
+
+        public ObservableCollection<TreeNode> Nodes { get; set;  } = new ObservableCollection<TreeNode> ();
+        private Dictionary<ModelBase, TreeNode> _nodeMap = new Dictionary<ModelBase, TreeNode> ();
 
         public SchoolYear AddSchoolYear(string schoolYearName)
         {
@@ -53,6 +58,87 @@ namespace MySchoolScoreManagementUI.Domain
             cr4.AddStudent("김수현");
             cr4.AddStudent("박신혜");
 
+        }
+
+        private void SchoolYearCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch( e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    foreach (SchoolYear schoolYear in e.NewItems)
+                    {
+                        var node = new TreeNode(schoolYear);
+                        Nodes.Add(node);
+                        _nodeMap[schoolYear] = node;
+                        schoolYear.ClassRooms.CollectionChanged += ClassRoomCollectionChanged;
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (SchoolYear schoolYear in e.OldItems)
+                    {
+                        var node = _nodeMap[schoolYear];
+
+                        if (node != null)
+                        {
+                            Nodes.Remove(node);
+                            _nodeMap.Remove(schoolYear);
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private void ClassRoomCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch(e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    foreach (ClassRoom classRoom in e.NewItems)
+                    {
+                        var node = new TreeNode(classRoom);
+                        _nodeMap[classRoom] = node;
+                        _nodeMap[classRoom.SchoolYear].AddSubNode(node);
+                        classRoom.Students.CollectionChanged += StudentCollectionChanged;
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (ClassRoom classRoom in e.OldItems)
+                    {
+                        var node = _nodeMap[classRoom];
+                        if (node != null)
+                        {
+                            _nodeMap[classRoom.SchoolYear].RemoveSubNode(node);
+                            _nodeMap.Remove(classRoom);
+                        }
+                    }
+                    break;
+                }
+            }
+
+        private void StudentCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    foreach (Student student in e.NewItems)
+                    {
+                        var node = new TreeNode(student);
+                        _nodeMap[student] = node;
+                        _nodeMap[student.ClassRoom].AddSubNode(node);
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (Student student in e.OldItems)
+                    {
+                        var node = _nodeMap[student];
+                        if (node != null)
+                        {
+                            _nodeMap[student.ClassRoom].RemoveSubNode(node);
+                            _nodeMap.Remove(student);
+                        }
+                    }
+                    break;
+            }
         }
     }
 }
---
+++ MySchoolScoreManagementUI/Domain/TreeNode.cs
@@ -0,0 +1,60 @@
+﻿using DevExpress.Xpf.Grid;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySchoolScoreManagementUI.Domain
+{
+    public class TreeNode : ModelBase
+    {
+        public TreeNode(ModelBase model)
+        {
+            _model = model;
+            Name = model.Name;
+        }
+
+        public ObservableCollection<TreeNode> SubNodes { get; set; } = new ObservableCollection<TreeNode>();
+
+        private ModelBase _model;
+
+        public ModelBase Model
+        {
+            get => _model;
+            set => SetProperty(field: ref _model, value);
+        }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set => SetProperty(ref _isSelected, value);
+        }
+
+        public void AddSubNode(TreeNode child)
+        {
+            if (!SubNodes.Contains(child))
+                SubNodes.Add(child);
+        }
+
+        public void RemoveSubNode(TreeNode child)
+        {
+            if (SubNodes.Contains(child))
+                SubNodes.Add(child);
+        }
+    }
+
+    public class TreeChildSelector : IChildNodesSelector
+    {
+        public IEnumerable SelectChildren(object item)
+        {
+            if (item is TreeNode node)
+                return node.SubNodes;
+
+            return null;
+        }
+    }
+ }
